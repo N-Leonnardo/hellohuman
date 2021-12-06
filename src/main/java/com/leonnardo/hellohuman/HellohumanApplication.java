02414c49b3ba5a16354330f37afb8fae6122e98e
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2021-12-06 15:56:09 -0800

    initial commit

---
+++ src/main/java/com/leonnardo/hellohuman/HellohumanApplication.java
@@ -0,0 +1,23 @@
+package com.leonnardo.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class HellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanApplication.class, args);
+	}
+	
+    @RequestMapping("/")
+    public String index(@RequestParam(value="firstname") String fname) {
+        return "Hello: " + fname;
+    }
+
+}
